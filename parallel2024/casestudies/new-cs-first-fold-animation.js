99a4d0fa121ce56117bfe0aca26f58060be145e8
suhelunparallel <157339345+suhelunparallel@users.noreply.github.com>
2024-05-23 19:15:08 +0530

    Create new-cs-first-fold-animation.js

---
+++ parallel2024/casestudies/new-cs-first-fold-animation.js
@@ -0,0 +1,60 @@
+const csDetailsWrapper = document.querySelector('[data-cs="details"]');
+const csVideoWrapper = document.querySelector("[data-cs='video-wrapper']");
+const { videoType } = csVideoWrapper.dataset;
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.set(csDetailsWrapper, { height: "100%", display: "block" });
+
+  function handleAnimationCompleteForDesktop(videoType) {
+    if (videoType === "video") {
+      const videoPlayer = csVideoWrapper.querySelector("video");
+      videoPlayer.play();
+    } else if (videoType === "lottie") {
+      const lottiePlayer = csVideoWrapper.querySelector(
+        '[data-lottie="player"]',
+      );
+      lottiePlayer.setAttribute("autoplay", "");
+      lottiePlayer.play();
+    } else {
+      return;
+    }
+  }
+
+  function handleVideoPlayForMobile(videoType) {
+    if (videoType === "video") {
+      const videoPlayer = csVideoWrapper.querySelector("video");
+      videoPlayer.setAttribute("autoplay", true);
+      videoPlayer.play();
+    } else if (videoType === "lottie") {
+      const lottiePlayer = csVideoWrapper.querySelector(
+        '[data-lottie="player"]',
+      );
+      lottiePlayer.setAttribute("autoplay", "");
+      lottiePlayer.addEventListener('ready', function(){
+      lottiePlayer.play();
+      })
+    } else {
+      return;
+    }
+  }
+
+  function animateSection() {
+    const initialY = `-${csDetailsWrapper.offsetHeight}`;
+
+    gsap.set(csVideoWrapper, { y: initialY });
+
+    gsap.to(csVideoWrapper, {
+      y: 0,
+      ease: "ease.in",
+      duration: 1.25,
+      onComplete: () => handleAnimationCompleteForDesktop(videoType),
+      delay: 1.25,
+    });
+  }
+
+  if (window.innerWidth > 991) {
+    animateSection();
+  } else {
+    handleVideoPlayForMobile(videoType);
+  }
+});
